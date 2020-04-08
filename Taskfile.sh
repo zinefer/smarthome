314ef0201cffc108b3927b24ca7e76dd5efa2279
zinefer <zinefer@gmail.com>
2020-04-07 23:49:59 -0600

    Create clear-hosts and import helper tasks

--- Taskfile.sh
+++ Taskfile.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function _forEachHostExec {
+    hosts="ansible/hosts"
+    while IFS= read -r line
+    do
+        [[ $line == [* ]] || [[ $line == "" ]] && continue
+        pieces=($line)
+        host=${pieces[0]}
+        ip=${pieces[1]#*=}
+
+        $1 $host $ip
+    done < "$hosts"
+}
+
 function install {
     # Terraform
     go get github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provider-proxmox
@@ -11,7 +24,47 @@ function install {
     ln -s $(which terraform-provider-proxmox) ~/.terraform.d/plugins/terraform-provider-proxmox
 
     # Ansible
+    galaxy
+}
+
+function galaxy {
     ansible-galaxy install -r ansible/requirements.yml --roles-path ansible/roles
+}
+
+function clear-hosts {
+    function clr {
+        ssh-keygen -f ~/.ssh/known_hosts -R $1
+        ssh-keygen -f ~/.ssh/known_hosts -R $2
+    }
+    
+    _forEachHostExec clr
+}
+
+function import {    
+    function imp {
+        fqdn=$1
+        ip=$2
+        host=${fqdn%.*}
+
+        [[ $host == "proxmox" ]] && return
+        
+        cat terraform/main.tf | grep "${host}_container" > /dev/null; isVm=$?
+
+        cmd="terraform import -config terraform module.${host}_"
+
+        if (( isVm )); then
+            cmd+="vm.proxmox_vm_qemu.vm"
+        else
+            cmd+="container.proxmox_lxc.container"
+        fi
+
+        id="1$(printf '%04d\n' ${ip##*.})"
+        cmd+=" proxmox/lxc/$id"
+
+        $cmd
+    }
+
+    _forEachHostExec imp
 }
 
 function provision {
@@ -29,11 +82,8 @@ function deploy {
 function help {
     echo "$0 <task> <args>"
     echo "Tasks:"
-    compgen -A function | cat -n
+    compgen -A function | sed -En '/_/!p' | cat -n
 }
 
 TIMEFORMAT="Task completed ins%3lR"
 time ${@:-help}
-
-
-
